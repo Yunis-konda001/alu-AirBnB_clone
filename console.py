83d74c81f6c24e0943c75e04f2ac5153a75049c1
Yunis-Konda001 <k.konda@alustudent.com>
2025-01-11 01:01:37 +0300

    sg

--- console.py
+++ console.py
@@ -6,11 +6,8 @@ This module provides a command-line interface for the HBNB project.
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """
-    A command-line interface for the HBNB project.
-    """
-
-    prompt = "(hbnb) "
+    """Define Hbnb command interpreter"""
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
         """
@@ -18,20 +15,15 @@ class HBNBCommand(cmd.Cmd):
         """
         return True
 
-    def help_quit(self, arg):
+    def help_quit(self):
         """
         Quit the application.
-
-        This method is called when the user enters the 'quit' command.
-        It returns True to exit the command loop.
         """
         print("Quit command to exit the program")
 
     def do_EOF(self, arg):
-        """
-        Handles the EOF (End Of File) signal.
-        """
-        print()
+        """EOF command to exit the program\n"""
+        print("")
         return True
 
 if __name__ == "__main__":
