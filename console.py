7fb3083ada0cbca4c64187cba44e7afc6775024d
Yunis-Konda001 <k.konda@alustudent.com>
2025-01-11 14:18:03 +0300

    sg

--- console.py
+++ console.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
 """
-This module provides a command-line interface for the HBNB project.
+This module contains the entry point of the command interpreter.
 """
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-    """Define Hbnb command interpreter"""
-    prompt = '(hbnb) '
+    """Command interpreter for AirBnB clone."""
+    
+    # Custom prompt
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """
-        Quit the application.
-        """
+        """Quit command to exit the program."""
         return True
-
-    def help_quit(self):
-        """
-        Quit the application.
-        """
-        print("Quit command to exit the program")
 
     def do_EOF(self, arg):
-        """EOF command to exit the program\n"""
-        print("")
+        """Exit the program with EOF signal."""
+        print()  # Print a newline before exiting
         return True
+
+    def emptyline(self):
+        """Override the default behavior to do nothing on an empty line."""
+        pass
+
+    def help_quit(self):
+        """Help information for quit command."""
+        print("Quit command to exit the program")
+
+    def help_EOF(self):
+        """Help information for EOF command."""
+        print("EOF command to exit the program")
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
