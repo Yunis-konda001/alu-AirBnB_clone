99f1feae26d2c743c1e27bfd9c0b6b39b940cf8b
larissanoella-05 <s.noella@alustudent.com>
2025-01-13 10:21:57 +0200

    sg

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,3 @@
-#Write a class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances
-#it will have this Private class attributes:
-# __file_path: string - path to the JSON file (ex: file.json)
-# __objects: dictionary - empty but will store all objects by <class name>.id (ex: to store a BaseModel object with id=12121212, the key will be BaseModel.12121212)    
-# Public instance methods:
-# all(self): returns the dictionary __objects
-# new(self, obj): sets in __objects the obj with key <obj class name>.id
-# save(self): serializes __objects to the JSON file (path: __file_path)
-# reload(self): deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
-
 import json
 import os
 from models.base_model import BaseModel
@@ -50,4 +40,4 @@ class FileStorage:
                 for key, value in json.load(file).items():
                     FileStorage.__objects[key] = BaseModel(**value)
         else:
-            pass    # No exception should be raised if the file doesn't exist
+            pass
