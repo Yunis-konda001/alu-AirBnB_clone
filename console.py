6065cc40fbcb9b15c3e1adf2757964b1590f34b0
larissanoella-05 <s.noella@alustudent.com>
2025-01-13 11:10:41 +0200

    initial

--- console.py
+++ console.py
@@ -1,1 +1,132 @@
- 
+#!/usr/bin/python3
+"""
+This module contains the entry point of the command interpreter.
+"""
+
+import cmd
+from models.base_model import BaseModel
+from models.user import User
+from models import storage
+
+# List of valid classes
+classes = {"BaseModel": BaseModel, "User": User}
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for AirBnB clone."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program with EOF signal."""
+        print()
+        return True
+
+    def emptyline(self):
+        """Override the default behavior to do nothing on an empty line."""
+        pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it, and prints the id."""
+        if not arg:
+            print("** class name missing **")
+            return
+        if arg not in classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = eval(arg)()
+        storage.save()
+        print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on class name and id."""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        obj = storage.all().get(key)
+        if not obj:
+            print("** no instance found **")
+        else:
+            print(obj)
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id."""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        if key not in storage.all():
+            print("** no instance found **")
+        else:
+            del storage.all()[key]
+            storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representations of all instances."""
+        objects = storage.all()
+        if not arg:
+            print([str(obj) for obj in objects.values()])
+            return
+        if arg not in classes:
+            print("** class doesn't exist **")
+            return
+        print([str(obj) for key, obj in objects.items() if key.startswith(arg)])
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or updating attribute.
+        Usage: update <class name> <id> <attribute name> "<attribute value>"
+        """
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        obj = storage.all().get(key)
+        if not obj:
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attr_name = args[2]
+        attr_value = args[3].strip('"')
+        # Cast the value to the correct type
+        if attr_value.isdigit():
+            attr_value = int(attr_value)
+        elif attr_value.replace('.', '', 1).isdigit():
+            attr_value = float(attr_value)
+        setattr(obj, attr_name, attr_value)
+        obj.save()
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop() 
