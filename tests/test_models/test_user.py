14929c19fa2681b839e28916efb4274782d8ceb3
Yunis-Konda001 <k.konda@alustudent.com>
2025-01-13 10:53:58 +0300

    sg

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""Test cases for the User class."""
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
@@ -14,21 +14,21 @@ class test_User(test_basemodel):
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """ The first name should be a string """
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
+        """ The last name should be a string """
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
+        """ The email should be a string """
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
+        """ The password should be a string """
         new = self.value()
         self.assertEqual(type(new.password), str)
